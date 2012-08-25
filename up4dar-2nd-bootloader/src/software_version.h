8dec47ab2cad7190684795af9b05a64ceb90b129
Michael Dirska <dl1bff@mdx.de>
2012-08-25 23:10:10 +0200

    add version bytes at 0x80002004, generate binary file form elf file

---
+++ up4dar-2nd-bootloader/src/software_version.h
@@ -0,0 +1,46 @@
+/*
+
+Copyright (C) 2012   Michael Dirska, DL1BFF (dl1bff@mdx.de)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/*
+ * software_version.h
+ *
+ * Created: 25.08.2012 18:50:52
+ *  Author: mdirska
+ */ 
+
+
+#ifndef SOFTWARE_VERSION_H_
+#define SOFTWARE_VERSION_H_
+
+#define SOFTWARE_IMAGE_PHY			1
+#define SOFTWARE_IMAGE_UPDATER		2
+#define SOFTWARE_IMAGE_SYSTEM		3
+
+#define SOFTWARE_MATURITY_NORMAL		0x00
+#define SOFTWARE_MATURITY_BETA			0x80
+#define SOFTWARE_MATURITY_EXPERIMENTAL	0x40
+
+
+#define SWVER_BYTE0		(SOFTWARE_IMAGE_UPDATER | SOFTWARE_MATURITY_EXPERIMENTAL)
+#define SWVER_BYTE1		1
+#define SWVER_BYTE2		0
+#define SWVER_BYTE3		2
+
+
+#endif /* SOFTWARE_VERSION_H_ */
