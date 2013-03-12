10f697a8ef81e8e25451d8072fbf3f71b644dcac
performer <performer@smart.cyanidehouse.ru>
2013-03-12 23:04:15 +0400

    Copyright added

--- up4dar-os/src/up_dstar/aprs.h
+++ up4dar-os/src/up_dstar/aprs.h
@@ -1,12 +1,27 @@
+/*
+
+Copyright (C) 2013   Artem Prilutskiy, R3ABM (r3abm@dstar.su)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #ifndef APRS_H
 #define APRS_H
 
 #include "FreeRTOS.h"
 #include "gcc_builtin.h"
-
-// !! IMPORTANT !!!!
-// Please add next settings:
-
 
 void aprs_process_gps_data(const char** parameters);
 size_t aprs_get_slow_data(uint8_t* data);
